84ef37a0940ce4217f31afb8b1f85919ac4c5f76
KarthikUllal <karthik2003ku@email.com>
2025-07-11 23:12:24 +0530

    created necessery folders and files

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#create db obj globally
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///todo.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app) #linking db obj to the actual app
+
+
+    from app.routes.auth import auth_bp
+    from app.routes.auth import tasks_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(tasks_bp)
+
+    return app
