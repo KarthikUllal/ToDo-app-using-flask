5141ea9adce24973b6b211098cc9af92a83454b8
KarthikUllal <karthik2003ku@email.com>
2025-07-12 21:36:51 +0530

    backend logic for viewing , adding and clearing tasks

---
+++ app/routes/task.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template,session, redirect, request, url_for, flash
+from app import db
+from app.models import Task
+
+
+tasks_bp = Blueprint('task', __name__)
+
+@tasks_bp.route('/')
+def view_tasks(): #To show all the task of logged in user
+    if 'user' not in session:
+        return redirect(url_for('auth.login'))
+    tasks = Task.query.all()
+    return render_template('task.html', tasks = tasks)
+
+
+@tasks_bp.route('/add', methods = ['POST'])
+def add_task():
+    if 'user' not in session:
+        return redirect(url_for('auth.login'))
+
+    title = request.form.get('title')
+    if title :
+        new_task = Task(title = title, status = 'Pending')
+        db.session.add(new_task)
+        db.session.commit()
+        flash('Task Added Successfully', 'success')
+
+    return redirect(url_for('tasks.view_tasks'))
+
+
+@tasks_bp.route('/toggle/<int:task_id', methods = ["POST"])
+def toggle_status(task_id):
+    task = Task.query(task_id)
+    if task:
+        if task.status == 'Pending':
+            task.status = 'Working'
+        elif task.status == 'Working':
+            task.status = 'Done'
+        else:
+            task.status = 'Pending'
+    return redirect(url_for('tasks.view_tasks'))
+
+
+@tasks_bp.route('/clear', methods = ["POST"])
+def clear():
+    Task.query.delete()
+    db.session.commit()
+    flash('All Tasks Cleared', 'info')
+    return redirect(url_for('tasks.view_tasks'))
